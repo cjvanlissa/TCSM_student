01e233e9466d021ad7347c7722a3dbb5ded1420f
cjvanlissa <c.j.vanlissa@uu.nl>
2020-08-20 10:39:07 +0200

    Add data

---
+++ run_me.R
@@ -0,0 +1,13 @@
+# Run these commands only once to install packages:
+install.packages("tidySEM", dependencies = TRUE)
+install.packages("foreign", dependencies = TRUE)
+install.packages("readxl", dependencies = TRUE)
+install.packages("semTools", dependencies = TRUE)
+
+# Run these commands every time to load the installed packages:
+library(tidySEM)
+library(foreign)
+library(readxl)
+library(semTools)
+
+# Then, write your analysis code in a script below
